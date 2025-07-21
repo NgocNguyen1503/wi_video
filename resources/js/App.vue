d692755c875aff64bda6be4be026327b79827724
NgocNguyen1503 <feyd153@gmail.com>
2025-07-21 17:40:16 +0700

    Add Chat and Content components

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,17 @@
 <template>
-    <RouterView />
+    <ContentComponent />
+    <ChatComponent />
+
 </template>
+
+<script>
+import ContentComponent from './components/ContentComponent.vue';
+import ChatComponent from './components/ChatComponent.vue';
+
+export default {
+    components: {
+        ContentComponent,
+        ChatComponent
+    }
+}
+</script>
