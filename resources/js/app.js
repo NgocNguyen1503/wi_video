4ff0e7d238af40d60aa37297c2fc54e0c732439b
NgocNguyen1503 <feyd153@gmail.com>
2025-07-21 17:39:49 +0700

    Remove vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,10 @@ import "./bootstrap";
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 
 const app = createApp(App);
 
-app.use(router);
+// app.use(router);
 
 app.mount("#app");
